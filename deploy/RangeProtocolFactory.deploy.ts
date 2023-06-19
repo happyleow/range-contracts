abe353fdb401eb75917bb8e2863bfceaf4f75f94
0xbateman <0xbateman@rangeprotocol.com>
2023-06-19 14:37:17 +0800

    add deployment script for thena

--- deploy/RangeProtocolFactory.deploy.ts
+++ deploy/RangeProtocolFactory.deploy.ts
@@ -3,12 +3,12 @@ import { LedgerSigner } from "@anders-t/ethers-ledger";
 async function main() {
   const provider = ethers.getDefaultProvider("");
   const ledger = await new LedgerSigner(provider, "");
-  const ALGEBRA_FACTORY = "0x411b0fAcC3489691f28ad58c47006AF5E3Ab3A28";
+  const THENA_FACTORY = "0x306F06C147f064A010530292A1EB6737c3e378e4";
   let RangeProtocolFactory = await ethers.getContractFactory(
     "RangeProtocolFactory"
   );
   RangeProtocolFactory = await RangeProtocolFactory.connect(ledger);
-  const factory = await RangeProtocolFactory.deploy(ALGEBRA_FACTORY);
+  const factory = await RangeProtocolFactory.deploy(THENA_FACTORY);
   console.log("Factory: ", factory.address);
 }
 // We recommend this pattern to be able to use async/await everywhere
